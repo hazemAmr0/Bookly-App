e8e534acb8497748931cc29ca7deb5dcc491f059
hazemAmr0 <109316536+hazemAmr0@users.noreply.github.com>
2024-01-26 01:08:22 +0200

    refactor splash_view_body&Navigate to home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,3 @@
 import 'package:flutter/material.dart';
 const kPrimaryColor=Color(0xff100B20);
+const kPrimaryDuration=Duration(milliseconds: 250);
--- lib/features/Splash/presentation/Views/Widgets/Splash_View_Body.dart
+++ lib/features/Splash/presentation/Views/Widgets/Splash_View_Body.dart
@@ -1,7 +1,12 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/Home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/Splash/data/utils/assets.dart';
 import 'package:bookly_app/features/Splash/presentation/Views/Widgets/sliding_image.dart';
 import 'package:bookly_app/features/Splash/presentation/Views/Widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,13 +22,13 @@ late  Animation<Offset> slidingAnimation2;
   @override
   void initState() {
     super.initState();
-   
-    animationController = AnimationController(vsync: this,duration: const Duration(seconds: 2) );
-   
-   slidingAnimation=Tween<Offset>(begin: const Offset(3, 0), end: const Offset(0, 0)).animate(animationController);
-   slidingAnimation2=Tween<Offset>(begin: const Offset(-3, 0), end: const Offset(0, 0)).animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    NavigateToHome();
   }
+
+ 
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +42,24 @@ late  Animation<Offset> slidingAnimation2;
         ],
       ),
     );
+  }
+  void NavigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fadeIn, duration: kPrimaryDuration);
+    });
+  }
+    void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(3, 0), end: const Offset(0, 0))
+            .animate(animationController);
+    slidingAnimation2 =
+        Tween<Offset>(begin: const Offset(-3, 0), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
   }
 }
 
